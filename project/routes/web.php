9755cc8ff7f0d3f0bb289813faf991b9462b9a35
kokos <vladosshemetov@gmail.com>
2023-05-08 16:32:43 +0300

    reg

--- project/routes/web.php
+++ project/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/', 'welcome');
+Route::view('/dashboard', 'dashboard');
+Route::view('register', 'auth.register');
